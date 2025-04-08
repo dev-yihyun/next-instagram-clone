755b104fb118d9773b29b4bc4ec8800d12d0d5d3
dev-yihyun <dev_yihyun@naver.com>
2025-04-09 01:00:34 +0900

    [feat] 사용자 이미지

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 import { CiSquarePlus } from "react-icons/ci";
 import { FaHome } from "react-icons/fa";
@@ -8,13 +9,15 @@ import { IoSearch } from "react-icons/io5";
 
 function Navbar() {
     const { data: session } = useSession();
+    console.log(session);
+    // console.log(session?.user?.image);
     return (
         <>
-            <div className="border-b border-gray-500 flex flex-row justify-between justify-items-center items-center p-4 mb-5">
+            <div className="border-b border-gray-500 flex flex-row justify-between justify-items-center items-center p-4 mb-5 ">
                 <h1 className="text-4xl font-bold p-1 rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 bg-clip-text text-transparent">
                     <Link href="/">Instagram</Link>
                 </h1>
-                <div className=" flex flex-row gap-4 text-xl justify-items-center items-center">
+                <div className=" flex flex-row gap-4 text-2xl justify-items-center items-center">
                     <Link href="/">
                         <FaHome />
                     </Link>
@@ -24,16 +27,33 @@ function Navbar() {
                     <Link href="/new">
                         <CiSquarePlus />
                     </Link>
-
+                    {session ? (
+                        <div
+                            className="bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 w-[50px] h-[50px] flex items-center justify-center rounded-full
+"
+                        >
+                            <Image
+                                src={session?.user?.image || ""}
+                                width={40}
+                                height={40}
+                                alt="test"
+                                className="rounded-full"
+                            />
+                        </div>
+                    ) : (
+                        <></>
+                    )}
                     <Link href="/auth">
                         <div className=" flex items-center p-2 rounded-md bg-blue-500 hover:bg-blue-700">
                             {session ? (
-                                <button
-                                    className="text-base font-bold text-white cursor-pointer"
-                                    onClick={() => signOut()}
-                                >
-                                    Sign out
-                                </button>
+                                <>
+                                    <button
+                                        className="text-base font-bold text-white cursor-pointer"
+                                        onClick={() => signOut()}
+                                    >
+                                        Sign out
+                                    </button>
+                                </>
                             ) : (
                                 <button
                                     className="text-base font-bold text-white cursor-pointer"

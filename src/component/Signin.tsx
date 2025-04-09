1694d9066043ef78490cb73c7907efac5bede5e2
dev-yihyun <dev_yihyun@naver.com>
2025-04-10 00:13:35 +0900

    [design] google 로그인 디자인 수정

--- src/component/Signin.tsx
+++ src/component/Signin.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ClientSafeProvider, signIn } from "next-auth/react";
-
 type Props = {
     providers: Record<string, ClientSafeProvider>;
     callbackUrl: string;
@@ -10,13 +9,21 @@ type Props = {
 function Signin({ providers, callbackUrl }: Props) {
     return (
         <>
-            {Object.values(providers).map((provider) => (
-                <div key={provider.name}>
-                    <button onClick={() => signIn(provider.id, { callbackUrl })}>
-                        Sign in with {provider.name}
-                    </button>
-                </div>
-            ))}
+            <div className="flex justify-center items-center h-screen">
+                {Object.values(providers).map((provider) => (
+                    <div key={provider.name}>
+                        <button
+                            className="border border-gray-900 rounded-md text-xl  font-bold p-5
+                            
+                            shadow hover:shadow-lg transition-shadow duration-200
+                            cursor-pointer"
+                            onClick={() => signIn(provider.id, { callbackUrl })}
+                        >
+                            Sign in with {provider.name}
+                        </button>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }

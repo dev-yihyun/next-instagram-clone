d747bc57db530d5f566a065affe5ea5a7fbe2c47
dev-yihyun <dev_yihyun@naver.com>
2025-04-14 00:56:32 +0900

    [feat] ProfileImage 수정
    [description]
    - imagesize에 따른 ui 변경

--- src/component/ProfileImage.tsx
+++ src/component/ProfileImage.tsx
@@ -1,20 +1,32 @@
+type ImgaeSize = "small" | "medium" | "large";
 type Props = {
-    imageurl: string;
+    imageurl: string | null;
+    imagesize: ImgaeSize;
 };
 
-function ProfileImage({ imageurl }: Props) {
+function ProfileImage({ imageurl, imagesize = "large" }: Props) {
     return (
         <>
-            <div className="bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 w-[50px] h-[50px] flex items-center justify-center rounded-full aspect-square">
+            <div className={Container(imagesize)}>
                 <img
                     alt="user profile"
-                    src={imageurl}
+                    src={imageurl ?? undefined}
                     className="w-[40px] h-[40px] rounded-full"
                     referrerPolicy="no-referrer"
                 />
             </div>
         </>
     );
+}
+function Container(imgsize: string) {
+    const baseClass =
+        " w-[50px] h-[50px] flex items-center justify-center rounded-full aspect-square";
+    if (imgsize === "large") {
+        return `${baseClass} bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300`;
+    } else if (imgsize === "small") {
+        return `${baseClass}`;
+    }
+    return baseClass;
 }
 
 export default ProfileImage;

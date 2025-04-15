708f257a84219c52da0e09f18cb27a7d50c70e0f
dev-yihyun <dev_yihyun@naver.com>
2025-04-15 19:06:50 +0900

    [refactor] FollowingBar refactor

--- src/component/FollowingBar.tsx
+++ src/component/FollowingBar.tsx
@@ -1,26 +1,24 @@
 "use client";
 
+import { DetailUser, SimpleUser } from "@/model/user";
 import Link from "next/link";
 import { ClipLoader } from "react-spinners";
 import useSWR from "swr";
 import ProfileImage from "./ProfileImage";
 
-type User = {
-    image: string;
-    username: string;
-};
-
 function FollowingBar() {
-    const { data, error, isLoading } = useSWR(`/api/me`);
+    const { data, error, isLoading } = useSWR<DetailUser>(`/api/me`);
     const followings = data?.following ?? [];
 
     return (
         <>
-            <div className="border rounded-lg border-gray-300 w-full flex justify-items-start items-center p-4 shadow-sm shadow-neutral-300 h-32 gap-5">
-                {error && <ErrorMessage />}
-                {isLoading && <LoadingSpinner />}
-                {!error && !isLoading && <FollowingList followings={followings} />}
-            </div>
+            <section>
+                <div className="border rounded-lg border-gray-300 w-full flex justify-items-start items-center p-4 shadow-sm shadow-neutral-300 h-32 gap-5">
+                    {error && <ErrorMessage />}
+                    {isLoading && <LoadingSpinner />}
+                    {!error && !isLoading && <FollowingList followings={followings} />}
+                </div>
+            </section>
         </>
     );
 }
@@ -39,14 +37,14 @@ function LoadingSpinner() {
     );
 }
 
-function FollowingList({ followings }: { followings: User[] }) {
+function FollowingList({ followings }: { followings: SimpleUser[] }) {
     if (followings.length === 0) {
         return <p className="text-gray-400">No followings yet.</p>;
     }
     return followings.map((user) => (
         <div key={user?.username} className="flex flex-col items-center min-w-[72px]">
             <Link href={`/user/${user?.username}`}>
-                <ProfileImage imagesize="large" imageurl={user?.image} />
+                <ProfileImage imagesize="large" imageurl={user?.image ?? ""} />
             </Link>
             <p className="mt-1 text-sm">{user?.username}</p>
         </div>

23ceeb9efd6e5017350722028857271415e0a69d
dev-yihyun <dev_yihyun@naver.com>
2025-04-17 20:04:55 +0900

    [feat] Toggle Button
    [Description]
    - 좋아요 토글
    - 북마크 토글

--- src/component/ActionBar.tsx
+++ src/component/ActionBar.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { format } from "timeago.js";
+import BookmarkFillIcon from "./ui/icons/BookmarkFillIcon";
 import BookmarkIcon from "./ui/icons/BookmarkIcon";
+import HeartFillIcon from "./ui/icons/HeartFillIcon";
 import HeartIcon from "./ui/icons/HeartIcon";
+import ToggleButton from "./ui/ToggleButton";
 type Props = {
     likes: string[];
     username: string;
@@ -12,11 +16,23 @@ export function parseDate(date: string) {
     return format(date);
 }
 export default function ActionBar({ likes, username, text, createdAt }: Props) {
+    const [liked, setLiked] = useState(false);
+    const [bookmarked, setBookmarked] = useState(false);
     return (
         <>
             <div className="flex justify-between my-2 px-4 text-2xl">
-                <HeartIcon />
-                <BookmarkIcon />
+                <ToggleButton
+                    toggled={liked}
+                    onToggle={setLiked}
+                    onIcon={<HeartFillIcon />}
+                    offIcon={<HeartIcon />}
+                />
+                <ToggleButton
+                    toggled={bookmarked}
+                    onToggle={setBookmarked}
+                    onIcon={<BookmarkFillIcon />}
+                    offIcon={<BookmarkIcon />}
+                />
             </div>
             <p className="text-sm font-bold  my-2 px-4">{`${likes?.length ?? 0} ${
                 likes?.length > 1 ? "likes" : "like"

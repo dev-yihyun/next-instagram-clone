24b1f7fa4137747c9bc03e746f444f4d4d64cec0
dev-yihyun <dev_yihyun@naver.com>
2025-04-21 01:07:31 +0900

    [feat] follow 기능 구현

--- src/component/FollowButton.tsx
+++ src/component/FollowButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function FollowButton({ user }: Props) {
     const { username } = user;
-    const { user: loggedInUser } = useMe();
+    const { user: loggedInUser, toggleFollow } = useMe();
 
     const showButton = loggedInUser && loggedInUser.username !== username;
     const following =
@@ -17,5 +17,11 @@ export default function FollowButton({ user }: Props) {
 
     const text = following ? "Unfollow" : "Follow";
 
-    return <>{showButton && <Button text={text} onClick={() => {}} red={text === "Unfollow"} />}</>;
+    const handleFollow = () => {
+        toggleFollow(user.id, !following);
+    };
+
+    return (
+        <>{showButton && <Button text={text} onClick={handleFollow} red={text === "Unfollow"} />}</>
+    );
 }

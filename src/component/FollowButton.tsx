e9b6933608cadd46fc84fc1ef98235c3f364371b
dev-yihyun <dev_yihyun@naver.com>
2025-04-21 01:28:03 +0900

    [fix] follow loading 추가

--- src/component/FollowButton.tsx
+++ src/component/FollowButton.tsx
@@ -1,6 +1,9 @@
 "use client";
 import useMe from "@/hook/me";
 import { ProfileUser } from "@/model/user";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import { PulseLoader } from "react-spinners";
 import Button from "./ui/Button";
 
 type Props = {
@@ -10,6 +13,10 @@ type Props = {
 export default function FollowButton({ user }: Props) {
     const { username } = user;
     const { user: loggedInUser, toggleFollow } = useMe();
+    const router = useRouter();
+    const [isPending, startTransition] = useTransition();
+    const [isFetching, setIsFetching] = useState(false);
+    const isUpdating = isPending || isFetching;
 
     const showButton = loggedInUser && loggedInUser.username !== username;
     const following =
@@ -17,11 +24,32 @@ export default function FollowButton({ user }: Props) {
 
     const text = following ? "Unfollow" : "Follow";
 
-    const handleFollow = () => {
-        toggleFollow(user.id, !following);
+    const handleFollow = async () => {
+        setIsFetching(true);
+        await toggleFollow(user.id, !following);
+        setIsFetching(false);
+        startTransition(() => {
+            router.refresh();
+        });
     };
 
     return (
-        <>{showButton && <Button text={text} onClick={handleFollow} red={text === "Unfollow"} />}</>
+        <>
+            {showButton && (
+                <div className="relative">
+                    {isUpdating && (
+                        <div className="absolute z-20 inset-0 flex justify-center items-center">
+                            <PulseLoader size={6} />
+                        </div>
+                    )}
+                    <Button
+                        disabled={isUpdating}
+                        text={text}
+                        onClick={handleFollow}
+                        red={text === "Unfollow"}
+                    />
+                </div>
+            )}
+        </>
     );
 }

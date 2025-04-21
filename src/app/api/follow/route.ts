517a8b3d77260df4e4888389c0610362ea543de3
dev-yihyun <dev_yihyun@naver.com>
2025-04-21 16:39:18 +0900

    [refactor] withSessionUser를 사용한 route 리팩토링

--- src/app/api/follow/route.ts
+++ src/app/api/follow/route.ts
@@ -1,23 +1,19 @@
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { follow, unfollow } from "@/service/user";
-import { getServerSession } from "next-auth";
+import { withSessionUser } from "@/util/session";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(req: NextRequest) {
-    const session = await getServerSession(authOptions);
-    const user = session?.user;
-    if (!user) {
-        return new Response("Authentication Error", { status: 401 });
-    }
-    const { id: targetId, follow: isFollow } = await req.json();
+    return withSessionUser(async (user) => {
+        const { id: targetId, follow: isFollow } = await req.json();
 
-    if (!targetId || isFollow === undefined) {
-        return new Response("Bad Request", { status: 400 });
-    }
+        if (!targetId || isFollow === undefined) {
+            return new Response("Bad Request", { status: 400 });
+        }
 
-    const request = isFollow ? follow : unfollow;
+        const request = isFollow ? follow : unfollow;
 
-    return request(user.id, targetId) //
-        .then((res) => NextResponse.json(res))
-        .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+        return request(user.id, targetId) //
+            .then((res) => NextResponse.json(res))
+            .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+    });
 }
